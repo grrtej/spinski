d05df4ba0e28ff186130b6384080c8a27814a51c
Gurtej Saini <grrtej@gmail.com>
2025-05-29 21:49:51 -0700

    hello pyramid

---
+++ main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000);
+camera.position.set(0, 5, 10);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const gridHelper = new THREE.GridHelper();
+scene.add(gridHelper);
+
+const pyramid = new THREE.Mesh(new THREE.TetrahedronGeometry(), new THREE.MeshNormalMaterial());
+scene.add(pyramid)
+
+function animate() {
+    pyramid.rotation.x += 0.01;
+    pyramid.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
