2edfa731e36ab97813643a3a4c0b557ded416deb
Gurtej Saini <grrtej@gmail.com>
2025-05-30 18:59:11 -0700

    implement sierpinski

--- main.js
+++ main.js
@@ -13,19 +13,61 @@ camera.position.set(0, 5, 10);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const gridHelper = new THREE.GridHelper();
-scene.add(gridHelper);
+for (let i = 0; i <= 10; i++) {
+    const gridVertical = new THREE.GridHelper(10);
+    const gridHorizontal = new THREE.GridHelper(10);
+    gridVertical.rotation.z = THREE.MathUtils.degToRad(90);
+    gridVertical.position.x = -5 + i;
+    gridHorizontal.position.y = -5 + i;
+    scene.add(gridVertical);
+    scene.add(gridHorizontal);
+}
 
-const axesHelper = new THREE.AxesHelper(10);
-axesHelper.position.set(-5, 0, - 5);
-scene.add(axesHelper);
+const axes = new THREE.AxesHelper(5);
+scene.add(axes);
 
-const pyramid = new THREE.Mesh(new THREE.TetrahedronGeometry(), new THREE.MeshNormalMaterial());
-scene.add(pyramid);
+function spinski(A, B, C) {
+    const depthMax = 5;
+    const triangles = [];
+
+    rec(A, B, C, 0);
+
+    return triangles;
+
+    function rec(A, B, C, depth) {
+        if (depth == depthMax) {
+            triangles.push(new THREE.Shape([A, B, C]));
+            return;
+        }
+
+        const midAB = A.clone().add(B).multiplyScalar(0.5);
+        const midBC = B.clone().add(C).multiplyScalar(0.5);
+        const midCA = C.clone().add(A).multiplyScalar(0.5);
+
+        rec(A, midAB, midCA, depth + 1);
+        rec(midAB, B, midBC, depth + 1);
+        rec(midCA, midBC, C, depth + 1);
+    }
+}
+
+const s = 10;
+const h = s * Math.sqrt(3) / 2;
+const A = new THREE.Vector2(-s / 2, -h / 2);
+const B = new THREE.Vector2(s / 2, -h / 2);
+const C = new THREE.Vector2(0, h / 2);
+
+const triangles = spinski(A, B, C);
+
+const fractal = new THREE.Group();
+const material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
+for (const triangle of triangles) {
+    const mesh = new THREE.Mesh(new THREE.ShapeGeometry(triangle), material);
+    fractal.add(mesh);
+}
+scene.add(fractal);
 
 function animate() {
-    pyramid.rotation.x += 0.01;
-    pyramid.rotation.y += 0.01;
+    fractal.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
